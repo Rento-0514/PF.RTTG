5200ce129f39cb249c3889a07d71149b06f66757
Rento-0514 <purin.0514@icloud.com>
2023-11-03 09:48:09 +0900

    微修正、消去できない

--- app/controllers/reservation_requests_controller.rb
+++ app/controllers/reservation_requests_controller.rb
@@ -7,7 +7,7 @@ class ReservationRequestsController < ApplicationController
 
   def create
     @reservation_request = ReservationRequest.new(reservation_request_params)
-    @reservation_request.customer_id = current_user.customer.id
+    @reservation_request.customer_id = params[:reservation_request][:customer_id]
     @reservation_request.hotel_id = params[:reservation_request][:hotel_id]
     if @reservation_request.save
       redirect_to @reservation_request
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,7 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
-import "jquery"
-import "popper.js"
-import "bootstrap"
+import $ from 'jquery';
+import 'popper.js'; // Popper.jsを正しくインポート
+import 'bootstrap';
+import { Turbo } from "@hotwired/turbo-rails"
