93fccc674c0f59800bf794dcc8d24379124a2ed0
Rento-0514 <purin.0514@icloud.com>
2023-11-03 14:42:46 +0900

    顧客検索実装

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,4 +1,8 @@
 class Customer < ApplicationRecord
   belongs_to :user
   has_many :reservation_requests
+
+  def self.ransackable_attributes(auth_object = nil)
+    ["allergy", "birthday", "created_at", "id", "memo", "name", "updated_at", "user_id"]
+  end
 end
