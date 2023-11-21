a052db84f7b466c09f723dd0b96a2356be406e40
Rento-0514 <purin.0514@icloud.com>
2023-11-21 16:04:30 +0900

    検証ツールのエラー消えた

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,6 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
-import Popper from './node_modules/popper.js/dist/popper.js';
-import 'bootstrap';
+//= require jquery3
+//= require popper
+//= require bootstrap-sprockets
